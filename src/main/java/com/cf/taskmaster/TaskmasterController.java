995d7ba207e845687695f193f851128500d2b9b5
STEPHEN CHU <stephen.chu530@gmail.com>
2019-07-05 10:36:20 -0700

    fix: added cors support and removed dead code

--- src/main/java/com/cf/taskmaster/TaskmasterController.java
+++ src/main/java/com/cf/taskmaster/TaskmasterController.java
@@ -5,13 +5,11 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.UsesSunHttpServer;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
+@CrossOrigin
 @RestController
 public class TaskmasterController {
     private DynamoDBMapper dynamoDBMapper;
